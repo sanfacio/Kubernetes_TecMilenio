9495361db5705132603a508df060e308b3b30444
Santos G Facio <sanfacio@gmail.com>
2024-10-15 20:08:28 -0600

    Added new test scenarios and dependencies

--- Tema9_GHA/test/app.test.js
+++ Tema9_GHA/test/app.test.js
@@ -8,3 +8,11 @@ test('suma 1 + 2 es igual a 3', () => {
 test('suma -1 + 1 es igual a 0', () => {
     expect(suma(-1, 1)).toBe(0);
 });
+
+test('Debería devolver 5 cuando se suman 2 y 3', () => {
+    expect(suma(2, 3)).toBe(5);
+});
+
+test('Debería devolver -1 cuando se suman 2 y -3', () => {
+    expect(suma(2, -3)).toBe(-1);
+});
